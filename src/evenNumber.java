64bedee90363b5da2e1d685b5f86c06f949bd28a
Pratik Raj Biresh <pratikraj0308@yahoo.com>
2022-06-07 15:01:08 +0530

    First change

--- src/evenNumber.java
+++ src/evenNumber.java
@@ -1,19 +1,7 @@
 
 public class evenNumber {
 	
-	static String evenNumbers(int n) {
-		int i = 0;
-		String result = "";
-		while (i <= n) {
-			if (i%2 == 0) {
-				result = result + i;
-				
-				if (i != n) {
-					result = result + ',';
-				}
-			}
-			i++;
-		}
+	
 	}
 		
 	
